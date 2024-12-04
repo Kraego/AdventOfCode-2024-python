61170cd94b46de34483a494a4c9e9b05655d3b8b
Thomas Kraxner <71735135+Kraego@users.noreply.github.com>
2024-12-04 20:34:22 +0000

    ain't nobody got time for that

--- Day04.py
+++ Day04.py
@@ -3,7 +3,7 @@ SEARCH_WORD = 'XMAS'
 def readin_data(path : str) -> list[str]:
     with open(path, 'r') as f:
         return [line.rstrip('\n') for line in f]
-    
+
 
 def horizontal_count(input : list[str]) -> int:
     count = 0
@@ -19,38 +19,44 @@ def vertical_count(input : list[str]) -> int:
     return horizontal_count(transposed)
 
 
-def _unfold_dioganals(input : list[str]) -> list[str]:
-    # (0,0) (1,1) (2,2) (3,3)
-    # (0,1) (2,2) (3,3)
-    # (0,2) (1,3)
-    # (0,3) 
+def _reduce_input(input : list[str]) -> list[str]:
+    return [[x for x in row[1:]] for row in input[1:]]
 
-    # (1,0) (2,1) (3,2)
-    # (1,1) (2,2) (3,3)
-    # (1,2) (2,3)
-    # (1,3)
-    unfold = list()
-    for y in range(len(input)):
-        y_idx = y
-        for x in range(len(input[y])):
-            print((y_idx,x))
-            y_idx += 1
-            if y_idx == len(input):
-                break
-        print()
-        #unfold.append(line)
+
+def _unfold_diagonal(input : list[str]) -> str:
+    if len(input[0]) == 0:
+        return ''
+    
+    unfold = ''
+    current = str(input[0][0])
+
+    if len(input) == 1 or len(input[0]) == 1:
+        return current
+    else:
+        unfold += current + _unfold_diagonal(_reduce_input(input))
+
     return unfold
 
 
+def _unfold_diagonals(input : list[str]) -> list[str]:
+    unfolds = list()
+
+    for r in range(len(input)):
+        for c in range(len(input[0])):
+            cutoff = len(input[0]) if r == 0 else r
+            shifted_input = [[x for x in row[c:cutoff:1]] for row in input[r:]]
+            unfolds.append(_unfold_diagonal(shifted_input))
+
+    return unfolds
+
 def dioganal_count(input : list[str]) -> int:
-    unfolded = _unfold_dioganals(input)
-    return horizontal_count(unfolded)
+    return horizontal_count(_unfold_diagonals(input))
+
             
-    
 if __name__ == '__main__':
     wordsearch = readin_data('Input/Day04.txt')
-    # count = horizontal_count(wordsearch)
-    # count += vertical_count(wordsearch)
-    count = dioganal_count(wordsearch)
+    count = horizontal_count(wordsearch)
+    count += vertical_count(wordsearch)
+    count += dioganal_count(wordsearch)
 
     print(f'Wordcount for "{SEARCH_WORD}" is {count}');
